ebf018c80a8ae4f4bc94f7148f9b7bc92d79e0f4
imden-win <568089002@qq.com>
2022-02-20 19:08:46 +0800

    攻击测试

--- assets/scripts/PlayerController.ts
+++ assets/scripts/PlayerController.ts
@@ -34,30 +34,26 @@ export class PlayerControl extends Component {
 
     move: Vec4 = new Vec4(0, 0, 0, 0)
     movePlay: boolean = false
+    attack: number = 0
 
     start () {
-        PhysicsSystem2D.instance.enable = true
-        PhysicsSystem2D.instance.debugDrawFlags = EPhysics2DDrawFlags.Aabb | 
-        EPhysics2DDrawFlags.Pair | 
-        EPhysics2DDrawFlags.CenterOfMass |
-        EPhysics2DDrawFlags.Joint |
-        EPhysics2DDrawFlags.Shape
-        resources.preload('images/zhanli', SpriteFrame)
         input.on(Input.EventType.KEY_DOWN, this.moveKeyDown, this)
         input.on(Input.EventType.KEY_UP, this.moveKeyUp, this)
         // 注册碰撞
         // 注册单个碰撞体的回调函数
-        // let collider = this.getComponent(BoxCollider2D);
-        // console.log("collider", collider)
-        // if (collider) {
-        //     collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
-        //     collider.on(Contact2DType.END_CONTACT, this.onEndContact, this);
-        // }
-        // 注册全局碰撞回调函数
-        if (PhysicsSystem2D.instance) {
-            PhysicsSystem2D.instance.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
-            PhysicsSystem2D.instance.on(Contact2DType.END_CONTACT, this.onEndContact, this);
+        let collider = this.getComponent(BoxCollider2D);
+        console.log("collider", collider)
+        if (collider) {
+            collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+            collider.on(Contact2DType.POST_SOLVE, this.onPostSolve, this);
+            collider.on(Contact2DType.PRE_SOLVE, this.onPreSolve, this);
+            collider.on(Contact2DType.END_CONTACT, this.onEndContact, this);
         }
+        // 注册全局碰撞回调函数
+        // if (PhysicsSystem2D.instance) {
+        //     PhysicsSystem2D.instance.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+        //     PhysicsSystem2D.instance.on(Contact2DType.END_CONTACT, this.onEndContact, this);
+        // }
 
     }
     onBeginContact(selfCollider: BoxCollider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
@@ -101,6 +97,24 @@ export class PlayerControl extends Component {
                     this.move.z = this.move.x
                 }
                 this.move.x = 1
+                break
+            case KeyCode.KEY_J:
+                this.stopRunAnimation()
+                if (this.attack == 0) {
+                    this.attack = 1
+                    const ani = this.getComponent(Animation)
+                    ani.play("gongji1")
+                    setTimeout(()=>{
+                        this.attack = 2
+                    }, 380)
+                } else if (this.attack == 2) {
+                    this.attack = 3
+                    const ani = this.getComponent(Animation)
+                    ani.play("gongji2")
+                    setTimeout(()=>{
+                        this.attack = 0
+                    }, 380)
+                }
                 break
             default:
                 this.move.x = 0
@@ -152,6 +166,8 @@ export class PlayerControl extends Component {
                 }
                 this.move.w = 0
                 break
+            case KeyCode.KEY_J:
+                break
             default:
                 this.move.x = 0
                 this.move.y = 0
@@ -163,50 +179,54 @@ export class PlayerControl extends Component {
 
     update (deltaTime: number) {
         // [4]
-        let b = false
-        if (this.move.x > 0) {
-            b = true
-            if (!this.movePlay) {
-                this.startRunAnimation()
+        if (this.attack == 1) {
+
+        }else if (this.attack == 0){
+            let b = false
+            if (this.move.x > 0) {
+                b = true
+                if (!this.movePlay) {
+                    this.startRunAnimation()
+                }
+                this.node.setScale(1, 1, 1)
+                // 计算将要移动是否超出范围
+                const pos = new Vec3(this.node.position.x + this.moveSpeed * deltaTime, this.node.position.y, 0)
+                if (this.canMove(pos)) {
+                    this.node.setPosition(pos)
+                }
+            } else if (this.move.x < 0) {
+                b = true
+                if (!this.movePlay) {
+                    this.startRunAnimation()
+                }
+                this.node.setScale(-1, 1, 1)
+                const pos = new Vec3(this.node.position.x - this.moveSpeed * deltaTime, this.node.position.y, 0)
+                if (this.canMove(pos)) {
+                    this.node.setPosition(pos)
+                }
             }
-            this.node.setScale(1, 1, 1)
-            // 计算将要移动是否超出范围
-            const pos = new Vec3(this.node.position.x + this.moveSpeed * deltaTime, this.node.position.y, 0)
-            if (this.canMove(pos)) {
-                this.node.setPosition(pos)
+            if (this.move.y > 0) {
+                b = true
+                if (!this.movePlay) {
+                    this.startRunAnimation()
+                }
+                const pos = new Vec3(this.node.position.x, this.node.position.y + (this.moveSpeed * deltaTime / 2), 0)
+                if (this.canMove(pos)) {
+                    this.node.setPosition(pos)
+                }
+            } else if (this.move.y < 0) {
+                b = true
+                if (!this.movePlay) {
+                    this.startRunAnimation()
+                }
+                const pos = new Vec3(this.node.position.x , this.node.position.y - (this.moveSpeed * deltaTime / 2), 0)
+                if (this.canMove(pos)) {
+                    this.node.setPosition(pos)
+                }
             }
-        } else if (this.move.x < 0) {
-            b = true
-            if (!this.movePlay) {
-                this.startRunAnimation()
+            if (!b) {
+                this.stopRunAnimation()
             }
-            this.node.setScale(-1, 1, 1)
-            const pos = new Vec3(this.node.position.x - this.moveSpeed * deltaTime, this.node.position.y, 0)
-            if (this.canMove(pos)) {
-                this.node.setPosition(pos)
-            }
-        }
-        if (this.move.y > 0) {
-            b = true
-            if (!this.movePlay) {
-                this.startRunAnimation()
-            }
-            const pos = new Vec3(this.node.position.x, this.node.position.y + (this.moveSpeed * deltaTime / 2), 0)
-            if (this.canMove(pos)) {
-                this.node.setPosition(pos)
-            }
-        } else if (this.move.y < 0) {
-            b = true
-            if (!this.movePlay) {
-                this.startRunAnimation()
-            }
-            const pos = new Vec3(this.node.position.x , this.node.position.y - (this.moveSpeed * deltaTime / 2), 0)
-            if (this.canMove(pos)) {
-                this.node.setPosition(pos)
-            }
-        }
-        if (!b) {
-            this.stopRunAnimation()
         }
     }
     startRunAnimation() {
@@ -217,12 +237,13 @@ export class PlayerControl extends Component {
     stopRunAnimation() {
         const ani = this.getComponent(Animation)
         ani.stop()
-        resources.load("images/zhanli", ImageAsset, (err: any, img) => {
+        resources.load("images/站立2", ImageAsset, (err: any, img) => {
             this.getComponent(Sprite).spriteFrame = SpriteFrame.createWithImage(img)
         })
         let collider = this.getComponent(BoxCollider2D)
-        collider.size.x = 18
-        collider.size.y = 57
+        collider.size.x = 38
+        collider.size.y = 100
+        collider.apply()
         this.movePlay = false
     }
     canMove(pos: Vec3) {
